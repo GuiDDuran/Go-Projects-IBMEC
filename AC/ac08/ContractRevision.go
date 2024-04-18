02f07b40c4e99eaaa45a2b57ffb01fed9493b142
GuiDDuran <guilherme.d.gea@gmail.com>
2024-04-18 20:11:32 -0300

    AC08

--- AC/ac08/ContractRevision.go
+++ AC/ac08/ContractRevision.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
-func main(){
+func main() {
+  for {
+    var digit byte
+    var num string
+	fmt.Scanf("%c %s\n", &digit, &num)
+    count := 0
+
+    if digit == '0' && num == "0" {
+      break
+    }
 	
+    for i := 0; i < len(num); i++ {
+      if num[i] == '0' {
+        if count != 0 {
+          fmt.Printf("%c", num[i])
+        }
+      } else if num[i] != digit {
+        fmt.Printf("%c", num[i])
+        count = 1
+      }
+    }
+
+    if count == 0 {
+      fmt.Println(0)
+    } else {
+      fmt.Println()
+    }
+  }
 }
