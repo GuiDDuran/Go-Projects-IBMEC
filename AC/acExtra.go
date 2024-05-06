aeb7cd5568732650263374ff6faa240ee3cb0ec3
GuiDDuran <guilherme.d.gea@gmail.com>
2024-05-06 10:46:33 -0300

    AC Extra

--- AC/acExtra.go
+++ AC/acExtra.go
@@ -13,7 +13,6 @@ type Fila struct {
 	tamanho int
 	inicio  *No
 	fim    *No
-	// topo    *No
 }
 
 func (f *Fila) percorre() {
@@ -54,48 +53,6 @@ func (f *Fila) remove() error {
 	f.tamanho--
 	return nil
 }
-
-// func (f *Fila) percorre() {
-// 	if f.topo == nil {
-// 		fmt.Println("Fila vazia")
-// 	} else {
-// 		no := f.topo
-// 		for no.prox != nil {
-// 			fmt.Print(no.valor, " -> ")
-// 			no = no.prox
-// 		}
-// 		fmt.Println(no.valor)
-// 	}
-// }
-
-// func (f *Fila) insere(novoValor int) {
-// 	novoNo := &No{valor: novoValor}
-
-// 	if f.topo == nil {
-// 		f.topo = novoNo
-// 	} else {
-// 		aux := f.topo
-// 		for aux.prox != nil {
-// 			aux = aux.prox
-// 		}
-// 		aux.prox = novoNo
-// 	}
-
-// 	f.tamanho++
-// }
-
-// func (f *Fila) remove() error {
-// 	if f.topo == nil {
-// 		return fmt.Errorf("Fila vazia")
-// 	}
-
-// 	aux := f.topo
-// 	f.topo = f.topo.prox
-// 	aux.prox = nil
-
-// 	f.tamanho--
-// 	return nil
-// }
 
 func main() {
 	fila := Fila{}
